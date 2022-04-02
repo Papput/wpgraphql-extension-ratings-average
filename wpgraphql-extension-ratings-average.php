849299a15e6c92d969aa1a44829f1eb77a5d8319
Gunnar Abrahamsson <gunnar.abrahamsson@gmail.com>
2022-04-02 15:14:18 +0200

    Get ratings amount

--- wpgraphql-extension-ratings-average.php
+++ wpgraphql-extension-ratings-average.php
@@ -11,21 +11,43 @@
 // require_once '../wp-postratings/wp-postratings.php';
 
 
+/**
+ * Rating logs table name
+ */
+global $wpdb;
+$wpdb->ratings = $wpdb->prefix . 'ratings';
+
+// Get avrage ratings
 add_action( 'graphql_register_types', 'graphql_extension_register_types' );
 
 function graphql_extension_register_types() {
 
     register_graphql_field( 'recept', 'ratings_average', [
-        'description' => __( 'This is just a example field' ),
+        'description' => __( 'avrage recept rating' ),
         'type' => 'int',
         'resolve' => function( $root, $args, $context, $info ) {
             // graphql_debug(['root' => $root, 'context' => $context, 'info' => $info]);
             //graphql_debug(['root' => $root -> databaseId]);
             // return get_post_meta($root -> databaseId, 'ratings_average');
-
+            
             $avrage = get_post_meta($root -> databaseId, 'ratings_average', true);
+            
             if($avrage == "") return null;
             return  (int) $avrage;
+        }
+        ] );
+        register_graphql_field( 'recept', 'ratings_amount', [
+            'description' => __( 'Total amount of ratings' ),
+            'type' => 'int',
+            'resolve' => function( $root, $args, $context, $info ) {
+                // graphql_debug(['root' => $root, 'context' => $context, 'info' => $info]);
+                //graphql_debug(['root' => $root -> databaseId]);
+                // return get_post_meta($root -> databaseId, 'ratings_average');
+                global $wpdb;
+                $post_id = $root -> databaseId;
+
+                $get_rated_amount = $wpdb->get_var( $wpdb->prepare( "SELECT COUNT(*) FROM {$wpdb->ratings} WHERE rating_postid = %d", $post_id) );
+                return  (int) $get_rated_amount;
         }
     ] );
 }
